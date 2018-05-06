516c96847723484ff0a29e70fed0ece091eafced
FerVT <fer.vargas.torres@gmail.com>
2018-05-06 17:28:46 -0500

    Change side nav to vertical bar. Add group incidents modal.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -10,6 +10,7 @@ import { ContextResource } from './../../common/interfaces/context-resource';
 import { AppError } from './../../common/errors/app-error';
 import { NotFoundError } from './../../common/errors/not-found-error';
 import { BadRequestError } from './../../common/errors/bad-request-error';
+import { GroupService } from '../../services/group/group.service';
 
 @Component({
   selector: 'navbar',
@@ -17,31 +18,43 @@ import { BadRequestError } from './../../common/errors/bad-request-error';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  @Output('courseInfoUpdated') courseInfoUpdated = new EventEmitter<Alert>();
+  @Output('courseInfoUpdated') courseInfoUpdated  = new EventEmitter<Alert>();
+  @Output('groupIncidentsUpdated') groupIncidentsUpdated = new EventEmitter<Alert>();
 
   isEditCourseInfoModalOpen:    boolean;
   isEditCourseInfoTextAreaOpen: boolean;
+  isGroupIncidentsModalOpen:    boolean;
+  isGroupIncidentsTextAreaOpen: boolean;
   isLoading:                    boolean;
   courseInfo:                   string;
   courseInfoEditable:           string;
+  groupIncidents:               string;
+  groupIncidentsEditable:       string;
   contextSelected:              any;
 
-  private updatedCourseAlert: Alert
+  private updatedCourseAlert: Alert;
+  private updatedGroupAlert:  Alert;
 
   constructor(
     private authService:   AuthService,
     private courseService: CourseService,
+    private groupService:  GroupService,
     private router:        Router,
     private route:         ActivatedRoute
   ) {}
 
   ngOnInit() {
     this.contextSelected = this.route.snapshot.params;
-    this.courseInfo = 'No hay información para el curso.'
+    this.courseInfo = 'No hay información para el curso.';
     
     this.updatedCourseAlert = {
       type:    'alert-success',
       message: 'La información del curso se actualizó correctamente.'
+    };
+
+    this.updatedGroupAlert = {
+      type:    'alert-success',
+      message: 'Las incidencias del grupo se actualizaron correctamente.'
     };
 
     this.courseService.getSingle(this.contextSelected.courseId)
@@ -49,6 +62,23 @@ export class NavbarComponent implements OnInit {
         courseInfo => {
           this.courseInfo = courseInfo;
           this.courseInfoEditable = this.courseInfo;
+        },
+        (error: AppError) => {
+          if(error instanceof NotFoundError)
+            return;
+
+          if(error instanceof BadRequestError)
+            return this.router.navigate(['/context']);
+
+          throw error;
+        }
+      );
+
+      this.groupService.getSingle(this.contextSelected.groupId)
+      .subscribe(
+        groupInfo => {
+          this.groupIncidents = groupInfo.incidents || 'No hay incidencias del grupo.';
+          this.groupIncidentsEditable = this.groupIncidents;
         },
         (error: AppError) => {
           if(error instanceof NotFoundError)
@@ -83,6 +113,29 @@ export class NavbarComponent implements OnInit {
     this.isEditCourseInfoTextAreaOpen = false;
     this.isEditCourseInfoModalOpen = false;
     this.courseInfoEditable = this.courseInfo;
+  }
+
+  updateGroupIncidents(): void {
+    let updatedGroupIncidents = {
+      incidents: this.groupIncidentsEditable
+    };
+
+    this.isLoading = true;
+    this.groupService.update(updatedGroupIncidents, this.contextSelected.groupId)
+      .finally(() => this.isLoading = false)
+      .subscribe(
+        updatedGroup => {
+          this.groupIncidents = updatedGroup.incidents;
+          this.closeGroupIncidentsModal();
+          this.groupIncidentsUpdated.emit(this.updatedGroupAlert);
+        }
+      );
+  }
+
+  closeGroupIncidentsModal(): void {
+    this.isGroupIncidentsTextAreaOpen = false;
+    this.isGroupIncidentsModalOpen = false;
+    this.groupIncidentsEditable = this.groupIncidents;
   }
 
   navigateWithContextParams(route: string): void {
